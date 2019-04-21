b0fa7251b9206cc19004bebbdfa7e5531e924904
mikechuhmar <miha1797@gmail.com>
2019-04-21 16:34:33 +0300

    Create Probability.cs

---
+++ DataStructures/DataStructures/Probability.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructures
+{
+    struct Probability
+    {
+        public double P;
+        public int index;
+        public Probability(int index,double P)
+        {
+            this.index = index;
+            this.P = P;
+        }
+    }
+}
