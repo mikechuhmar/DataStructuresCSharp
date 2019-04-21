a5fc19e5c7dda6e55401778c8cc5f1b978f761bc
mikechuhmar <miha1797@gmail.com>
2019-04-21 16:34:27 +0300

    Create Interval.cs

---
+++ DataStructures/DataStructures/Interval.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructures
+{
+    class Interval
+    {
+        public int index;
+        public double a;
+        public double b;
+        public Interval(int index, double a, double b)
+        {
+            this.index = index;
+            this.a = a;
+            this.b = b;
+        }
+        public bool Contains(double c)
+        {
+            return (c > a && c <= b);
+        }
+    }
+}
